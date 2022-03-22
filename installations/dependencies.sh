3021c590c1f2c78c7943556db3799fc3c133454a
piyush-312 <40489548+piyush-312@users.noreply.github.com>
2022-03-22 19:23:44 +0530

    Update dependencies.sh

--- installations/dependencies.sh
+++ installations/dependencies.sh
@@ -6,11 +6,11 @@ sudo apt-get update -y # To get the latest package lists
 sudo apt-get upgrade -y # To install the latest package 
 sudo apt-get install xorg -y
 sudo apt-get install build-essential -y
-sudo apt-get install xserver-xorg-core xorg-video-abi-23 -y
-sudo apt-get install xserver-xorg-video-intel -y
-sudo apt-get install mesa-utils  -y
 sudo apt-get install g++ libglu1-mesa-dev freeglut3 freeglut3-dev mesa-common-dev qt5-default -y
 sudo apt-get install curl gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev libjpeg-dev libglu1-mesa-dev freeglut3 freeglut3-dev mesa-common-dev libusb-1.0-0-dev libusb-1.0-0 libudev-dev -y
 sudo apt-get install libsdl1.2-dev libsdl2-dev -y
 
-echo "export LD_LIBRARY_PATH=$"LD_LIBRARY_PATH":`pwd`/../lib" >> ~/.bashrc 
+sudo apt-get install -y ros-melodic-laser-filters 
+sudo apt-get install -y ros-melodic-ira-laser-tools
+
+echo "export LD_LIBRARY_PATH=$"LD_LIBRARY_PATH":`pwd`/../lib" >> ~/.bashrc
