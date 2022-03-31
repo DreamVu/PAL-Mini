3cf045f813bf4256f63eb1548197c5cfa7f1b916
piyush <you@example.com>
2022-04-01 01:56:31 +0530

    cleaned up files

--- installations/setup_camera_data.sh
+++ installations/setup_camera_data.sh
@@ -1,4 +1,5 @@
 sudo cp -r data5/ /
 sudo cp -r data6/ /
 sudo chown -R $USER:$USER /data5 /data6
-sudo chown -R $USER:$USER /data5/lut/* /data6/lut/* /data5/fusion/* /data6/fusion/*
+sudo chown -R $USER:$USER /data5/lut/* /data6/lut/* /data6/cfm/* /data6/cfm/* /data5/fusion/* /data6/fusion/* /data5/fusion/fcm/* /data6/fusion/fcm/*
+
