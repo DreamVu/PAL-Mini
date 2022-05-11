7eed3dbca0585369dde16217b30e45b192556373
piyush <piyush_khandelwal@dreamvu.com>
2022-05-11 13:34:14 +0530

    added arm libs and ros pkg updated

--- code_samples/compile.sh
+++ code_samples/compile.sh
@@ -1,5 +1,7 @@
-g++ 004_range_scan.cpp ../lib/libPAL.so `pkg-config --libs --cflags opencv libusb-1.0`   -O3  -o 004_range_scan.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11
+ARCH=$(uname -p)
 
-
-
-
+if [ "$ARCH" = "x86_64" ]; then
+	g++ 004_range_scan.cpp ../lib/x86_64/libPAL.so `pkg-config --libs --cflags opencv libusb-1.0`   -O3  -o 004_range_scan.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11
+else
+	g++ 004_range_scan.cpp ../lib/arm64/libPAL.so /usr/src/tensorrt/bin/common/logger.o `pkg-config --libs --cflags opencv libusb-1.0`   -O3  -o 004_range_scan.out -I../include/ -lv4l2 -lpthread  -lcudart -L/usr/local/cuda/lib64 -lnvinfer -ludev -std=c++11
+fi
--- installations/ros_cmake.sh
+++ installations/ros_cmake.sh
@@ -1,4 +1,21 @@
-cat ../dreamvu_pal_navigation/cmake_template/header.txt > ../dreamvu_pal_navigation/CMakeLists.txt
+ARCH=$(uname -p)
+
+if [ "$ARCH" = "x86_64" ]; then
+	cat ../dreamvu_pal_navigation/cmake_template/x86_64/header.txt > ../dreamvu_pal_navigation/CMakeLists.txt
+else
+	cat ../dreamvu_pal_navigation/cmake_template/arm64/header.txt > ../dreamvu_pal_navigation/CMakeLists.txt
+fi
+
 echo "set(PAL_INCLUDE_DIR" `pwd`/../include ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
-echo "set(PAL_LIBRARY" `pwd`/../lib/libPAL.so ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
-cat ../dreamvu_pal_navigation/cmake_template/footer.txt >> ../dreamvu_pal_navigation/CMakeLists.txt
+
+if [ "$ARCH" = "x86_64" ]; then
+	echo "set(PAL_LIBRARY" `pwd`/../lib/x86_64/libPAL.so ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
+else
+	echo "set(PAL_LIBRARY" `pwd`/../lib/arm64/libPAL.so ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
+fi
+
+if [ "$ARCH" = "x86_64" ]; then
+	cat ../dreamvu_pal_navigation/cmake_template/x86_64/footer.txt >> ../dreamvu_pal_navigation/CMakeLists.txt
+else
+	cat ../dreamvu_pal_navigation/cmake_template/arm64/footer.txt >> ../dreamvu_pal_navigation/CMakeLists.txt
+fi
