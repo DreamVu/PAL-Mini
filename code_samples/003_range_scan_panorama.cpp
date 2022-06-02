45c44e6a80c2d54dd8ca33bda70cfb486cae5e02
piyush <you@example.com>
2022-06-02 18:42:28 +0530

    aded code samples

---
+++ code_samples/003_range_scan_panorama.cpp
@@ -0,0 +1,117 @@
+/*
+
+CODE SAMPLE # 003: Range scan panorama
+This code will grab the left panorama with range scan overlayed on it and would be displayed in a window using opencv
+
+
+>>>>>> Compile this code using the following command....
+
+./compile.sh
+
+>>>>>> Execute the binary file by typing the following command...
+
+./003_range_scan_panorama.out
+
+
+>>>>>> KEYBOARD CONTROLS:
+
+       ESC key closes the window
+       
+
+*/
+
+
+# include <stdio.h>
+
+# include <opencv2/opencv.hpp>
+
+# include "PAL.h"
+#include "TimeLogger.h"
+#include <time.h>
+#include <unistd.h>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{
+
+	namedWindow( "PAL Mini Range Scan", WINDOW_NORMAL ); // Create a window for display.
+
+	int width, height;
+	PAL::Mode mode = PAL::Mode::LASER_SCAN;
+
+	std::vector<int> camera_indexes{5};
+	
+	if(argc > 1) 
+		camera_indexes[0] = std::atoi(argv[1]);
+
+
+	PAL::Mode def_mode = PAL::Mode::LASER_SCAN;
+
+	char path[1024];
+	sprintf(path,"/data%d/",camera_indexes[0]);
+
+	char path2[1024];
+	sprintf(path2,"/data%d/",6);
+
+	PAL::SetPathtoData(path, path2);
+
+	if (PAL::Init(width, height, camera_indexes, &def_mode) != PAL::SUCCESS) //Connect to the PAL camera
+	{
+		cout<<"Init failed"<<endl;
+		return 1;
+	}
+	PAL::EnablePC(false);
+	usleep(1000000);
+
+	//discarding initial frames
+	std::vector<PAL::Data::ODOA_Data> discard;
+	for(int i=0; i<5;i++)
+		discard =  PAL::GrabRangeScanData();		
+
+	PAL::CameraProperties data;
+	PAL::Acknowledgement ack_load = PAL::LoadProperties("../Explorer/SavedPalProperties.txt", &data);
+
+	if(ack_load != PAL::SUCCESS)
+	{
+		cout<<"Error Loading settings! Loading default values."<<endl;
+	}
+
+	//width and height are the dimensions of each panorama.
+	//Each of the panoramas are displayed at otheir original resolution.
+	resizeWindow("PAL Mini Range Scan", width, height);
+
+	int key = ' ';
+
+	cout<<"Press ESC to close the window."<<endl;
+
+	Mat output = cv::Mat::zeros(height, width, CV_8UC3);
+
+	//Display the overlayed image
+	imshow( "PAL Mini Range Scan", output);
+
+	//27 = esc key. Run the loop until the ESC key is pressed
+	while(key != 27)
+	{
+
+		std::vector<PAL::Data::ODOA_Data> data;
+
+		data =  PAL::GrabRangeScanData();	
+
+		//Display the overlayed image
+		imshow( "PAL Mini Range Scan", data[0].marked_left);  
+
+		//Wait for the keypress - with a timeout of 1 ms
+
+		key = waitKey(1) & 255;
+
+	}
+
+	printf("exiting the application\n");
+	PAL::Destroy();
+
+   
+    return 0;
+}
+
--- code_samples/compile.sh
+++ code_samples/compile.sh
@@ -1,14 +1,26 @@
 ARCH=$(uname -p)
 NO_OF_GPU=$(lspci | grep -ci vga)
+GPU_LIBS=""
 
 if [ "$ARCH" = "x86_64" ]; 
 then
     if [ $NO_OF_GPU -eq 2 ]; 
     then
-	    g++ 004_range_scan.cpp ../lib/x86_64_gpu/libPAL.so `pkg-config --libs --cflags opencv libusb-1.0`   -O3  -o 004_range_scan.out -I../include/ -lv4l2 -lpthread  -ludev -L/usr/local/cuda/lib64 -lnvinfer -lcudart /usr/src/tensorrt/bin/common/logger.o -std=c++11 
+    	GPU_LIBS="-L/usr/local/cuda/lib64 -lnvinfer -lcudart /usr/src/tensorrt/bin/common/logger.o"	     
     else
-	    g++ 004_range_scan.cpp ../lib/x86_64/libPAL.so `pkg-config --libs --cflags opencv libusb-1.0`   -O3  -o 004_range_scan.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11
-	fi                
+    	GPU_LIBS=""
+    fi
+    
+	g++ 001_stereo_panorama.cpp ../lib/x86_64/libPAL.so `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 001_stereo_panorama.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11 $GPU_LIBS
+	g++ 002_depth_panorama.cpp ../lib/x86_64/libPAL.so `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 002_depth_panorama.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11 $GPU_LIBS
+	g++ 003_range_scan_panorama.cpp ../lib/x86_64/libPAL.so `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 003_range_scan_panorama.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11 $GPU_LIBS   
+    g++ 004_video_capture.cpp ../lib/x86_64/libPAL.so `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 004_video_capture.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11 $GPU_LIBS
+    g++ 005_camera_properties.cpp ../lib/x86_64/libPAL.so `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 005_camera_properties.out -I../include/ -lv4l2 -lpthread  -ludev -std=c++11 $GPU_LIBS
+    
 else
-	g++ 004_range_scan.cpp ../lib/arm64/libPAL.so /usr/src/tensorrt/bin/common/logger.o `pkg-config --libs --cflags opencv libusb-1.0`   -O3  -o 004_range_scan.out -I../include/ -lv4l2 -lpthread  -lcudart -L/usr/local/cuda/lib64 -lnvinfer -ludev -std=c++11
+	g++ 001_stereo_panorama.cpp ../lib/arm64/libPAL.so /usr/src/tensorrt/bin/common/logger.o `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 001_stereo_panorama.out -I../include/ -lv4l2 -lpthread  -lcudart -L/usr/local/cuda/lib64 -lnvinfer -ludev -std=c++11
+	g++ 002_depth_panorama.cpp ../lib/arm64/libPAL.so /usr/src/tensorrt/bin/common/logger.o `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 002_depth_panorama.out -I../include/ -lv4l2 -lpthread  -lcudart -L/usr/local/cuda/lib64 -lnvinfer -ludev -std=c++11
+	g++ 003_range_scan_panorama.cpp ../lib/arm64/libPAL.so /usr/src/tensorrt/bin/common/logger.o `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 003_range_scan_panorama.out -I../include/ -lv4l2 -lpthread  -lcudart -L/usr/local/cuda/lib64 -lnvinfer -ludev -std=c++11
+	g++ 004_video_capture.cpp ../lib/arm64/libPAL.so /usr/src/tensorrt/bin/common/logger.o `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 004_video_capture.out -I../include/ -lv4l2 -lpthread  -lcudart -L/usr/local/cuda/lib64 -lnvinfer -ludev -std=c++11
+	g++ 005_camera_properties.cpp ../lib/arm64/libPAL.so /usr/src/tensorrt/bin/common/logger.o `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 005_camera_properties.out -I../include/ -lv4l2 -lpthread  -lcudart -L/usr/local/cuda/lib64 -lnvinfer -ludev -std=c++11
 fi
