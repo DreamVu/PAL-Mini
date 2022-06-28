f69f554eaf8b9e0ca4972fbf082b811abcfa4836
piyush <you@example.com>
2022-06-28 16:13:28 +0530

    lib names and binaries updated

--- installations/ros_cmake.sh
+++ installations/ros_cmake.sh
@@ -9,9 +9,9 @@ fi
 echo "set(PAL_INCLUDE_DIR" `pwd`/../include ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
 
 if [ "$ARCH" = "x86_64" ]; then
-	echo "set(PAL_LIBRARY" `pwd`/../lib/x86_64/libPAL.so ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
+	echo "set(PAL_LIBRARY" `pwd`/../lib/x86_64/libPAL_MINI.so ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
 else
-	echo "set(PAL_LIBRARY" `pwd`/../lib/arm64/libPAL.so ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
+	echo "set(PAL_LIBRARY" `pwd`/../lib/arm64/libPAL_MINI.so ")" >> ../dreamvu_pal_navigation/CMakeLists.txt
 fi
 
 if [ "$ARCH" = "x86_64" ]; then
