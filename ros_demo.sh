a345a9045ca79f033420908eceac22adfe9222f1
Debashish Roy <debashish.roy1998@gmail.com>
2022-06-30 19:16:11 +0530

    Update ros_demo.sh
    Update ros_demo.sh based on new installation from deb files

--- ros_demo.sh
+++ ros_demo.sh
@@ -1,9 +1,6 @@
-cd installations 
-./ros_cmake.sh
-cp -r ../dreamvu_pal_navigation/ ~/catkin_ws/src/
+cp -r ~/DreamVu/PAL-Mini/dreamvu_pal_navigation/ ~/catkin_ws/src/
 cd ~/catkin_ws/
 source ~/catkin_ws/devel/setup.bash
 catkin_make
 source ~/catkin_ws/devel/setup.bash
 roslaunch dreamvu_pal_navigation scan_rviz.launch
-
