427b775f031b04324e792f0eabc74b97b4aa769e
You <you@gmail.com>
2022-07-28 16:32:24 +0530

    ros demo script fixed

--- ros_demo.sh
+++ ros_demo.sh
@@ -1,5 +1,14 @@
-cd ~/catkin_ws/
-source ~/catkin_ws/devel/setup.bash
-catkin_make
-source ~/catkin_ws/devel/setup.bash
-roslaunch dreamvu_pal_navigation scan_rviz.launch
+if [ -e /opt/ros/foxy/ ] 
+then
+	cd ~/catkin_ws/
+	colcon build
+	. ./install/setup.bash
+	colcon build
+	ros2 launch src/dreamvu_pal_navigation/launch/scan_rviz_launch.py
+else
+	cd ~/catkin_ws/
+	source ~/catkin_ws/devel/setup.bash
+	catkin_make
+	source ~/catkin_ws/devel/setup.bash
+	roslaunch dreamvu_pal_navigation scan_rviz.launch
+fi
